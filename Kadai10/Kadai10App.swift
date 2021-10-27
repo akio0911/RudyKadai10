457c5191ab16a5f889506fb37efe9ba1d7fa67c1
ak0327 <ca.fie.0220@gmail.com>
2021-10-27 20:17:29 +0900

    Initial Commit

---
+++ Kadai10/Kadai10App.swift
@@ -0,0 +1,15 @@
+//
+//  Kadai10App.swift
+//  Kadai10
+//
+
+import SwiftUI
+
+@main
+struct Kadai10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
